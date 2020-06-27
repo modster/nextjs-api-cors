833d2fb3cbdcf6acef93b2b8028e94c8b9d22388
Mike Greeff <unmodster@gmail.com>
2020-06-27 04:20:10 -0600

    futures market buy success

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,24 +1,25 @@
 const Binance = require ('node-binance-api')
-
-// I n s t a n t i a t e  T h e  E x c h a n g e  O b j e c t
-const binance = new Binance().options({
-  APIKEY: process.env.BINANCE_APIKEY,
-  APISECRET: process.env.BINANCE_SECRET,
-  useServerTime: true,
-  recvWindow: 4000,
-  verbose: false,
-  test: true, //------------------------------------------------------test mode!
-  reconnect: true
-});
-
-// V a r i a b l e s
-const symbol = 'BTCUSDT'
-const qtyBtc = 0.05
+require('dotenv').config();
 
 export default async function handler(req, res) {
+    
+  // I n s t a n t i a t e  T h e  E x c h a n g e  O b j e c t
+  const binance = new Binance().options({
+    APIKEY: '58dpa4DvxQvEdGdsMQON3VueLTTR6qUUBZdTTcCGCNPnQkGH7umVH7vizgdG1vni',
+    APISECRET: 'hsTqehYpdBRd7t6rPjcAljMSpXuFKlPSJmQC18WnZfFynjL5LMgcpG9djDSKSTdg',
+    useServerTime: true,
+    recvWindow: 4000,
+    verbose: true,
+    test: false, //------------------------------------------------------test mode!
+    reconnect: true
+  });
+
+  // V a r i a b l e s
+  const symbol = 'BTCUSDT'
+  const qtyBtc = 0.05
 
   let result = await binance.futuresMarketBuy( symbol, qtyBtc )
-  console.log(result)
+  await console.log(result)
   
   res.statusCode = 200
   res.end()
