46df32c50f6751ed1eb684e0d4825e1a05cc9d0f
Mike Greeff <unmodster@gmail.com>
2020-06-27 09:48:02 -0600

    header module

--- pages/api/tradeHistory.js
+++ pages/api/tradeHistory.js
@@ -1,31 +1,33 @@
-const Binance = require ('node-binance-api')
-require('dotenv').config();
+const Binance = require('node-binance-api');
+const header = require('./header.js');
 
-// I n s t a n t i a t e  T h e  E x c h a n g e  O b j e c t :
+/**
+ *                  t r a d e H i s t o r y ( )
+ */
+
+ // B i n a n c e   C o n s t r u c t o r 
 const binance = new Binance().options({
-  APIKEY: '58dpa4DvxQvEdGdsMQON3VueLTTR6qUUBZdTTcCGCNPnQkGH7umVH7vizgdG1vni',
-  APISECRET: 'hsTqehYpdBRd7t6rPjcAljMSpXuFKlPSJmQC18WnZfFynjL5LMgcpG9djDSKSTdg',
+  APIKEY: "58dpa4DvxQvEdGdsMQON3VueLTTR6qUUBZdTTcCGCNPnQkGH7umVH7vizgdG1vni",
+  APISECRET: "hsTqehYpdBRd7t6rPjcAljMSpXuFKlPSJmQC18WnZfFynjL5LMgcpG9djDSKSTdg",
   useServerTime: true,
   recvWindow: 4000,
   verbose: true,
   test: false, //---------------------------------------------------test mode?
-  reconnect: true
+  reconnect: true,
 });
 
 // E x p o r t  F u n c t i o n
 export default function allTrades(req, res) {
+  var postData = req.body; // var postData = JSON.parse(req.body);
+  
+  // G e t  T r a d e s
+  binance.trades(postData.symbol, function (error, json) {
+    if (error) return console.error(error);
+    console.log( JSON.stringify( json ) );
+  });
 
-  // Retrieve the request's body and parse it as JSON
-  var postData = req.body;  // var postData = JSON.parse(req.body);
-  // console.log(postData);
-  
-  // G e t  T r a d e s 
-  binance.trades(postData.symbol, function(error, json) {
-    console.log( "Trade History:", json )
-  })
-  
-  // K e e p  O u r  P r o m i s e 
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(postData))
+  // K e e p  O u r  P r o m i s e
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(postData));
 }
