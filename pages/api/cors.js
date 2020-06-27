38c2714a64cb0d7a4b91c46ae5f3d6f1c92eeabc
Mike Greeff <unmodster@gmail.com>
2020-06-27 03:56:51 -0600

    futures marketBuy endpoint

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,19 +1,25 @@
-import Cors from 'cors'
-import initMiddleware from '../../lib/init-middleware'
+const Binance = require ('node-binance-api')
 
-// Initialize the cors middleware
-const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
-  Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
-)
+// I n s t a n t i a t e  T h e  E x c h a n g e  O b j e c t
+const binance = new Binance().options({
+  APIKEY: process.env.BINANCE_APIKEY,
+  APISECRET: process.env.BINANCE_SECRET,
+  useServerTime: true,
+  recvWindow: 4000,
+  verbose: false,
+  test: true, //------------------------------------------------------test mode!
+  reconnect: true
+});
+
+// V a r i a b l e s
+const symbol = 'BTCUSDT'
+const qtyBtc = 0.05
 
 export default async function handler(req, res) {
-  // Run cors
-  await cors(req, res)
 
-  // Rest of the API logic
-  res.json({ message: 'Hello Everyone!' })
+  let result = await binance.futuresMarketBuy( symbol, qtyBtc )
+  console.log(result)
+  
+  res.statusCode = 200
+  res.end()
 }
